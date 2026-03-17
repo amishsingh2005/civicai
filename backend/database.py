92b158bc722cce62c41d0f69f80e67c9fcbc127b
adityasrivastava5098 <adityasrivastava08989@gmail.com>
2026-03-17 12:59:05 +0530

    add database.py

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+CLIENT_NAME = "civicai_db" # You can change this to your preferred database name
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[CLIENT_NAME]
+users_collection = db["users"]
